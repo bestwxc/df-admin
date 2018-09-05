1d16d735fb611e41e1d681b82497d0754d4fe971
wxc <wxc252@github.com>
2018-09-05 23:06:14 +0800

    treeNode控制器类

---
+++ src/main/java/net/df/module/admin/config/TreeNodeController.java
@@ -0,0 +1,68 @@
+package net.df.module.admin.config;
+
+import net.df.base.server.Result;
+import net.df.base.utils.MapUtils;
+import net.df.base.utils.ResultUtils;
+import net.df.module.admin.model.TreeNode;
+import net.df.module.admin.service.TreeNodeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/admin")
+public class TreeNodeController {
+
+    private Logger logger = LoggerFactory.getLogger(TreeNodeController.class);
+
+    @Autowired
+    private TreeNodeService treeNodeService;
+
+    /**
+     * 查询字典
+     * @param map
+     * @return
+     */
+    @RequestMapping("/list")
+    public Result<?> list(@RequestBody Map<String,?> map) {
+        Integer resultType = MapUtils.getIntegerFromMap(map, "resultType", 1);
+        Result<?> result = null;
+        switch (resultType){
+            case 1: result = listType(map);break;
+            case 2: result = listTypes(map);break;
+            default: break;
+        }
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 查询多种类型字典
+     * @param map
+     * @return
+     */
+    public Result<Map<String,List<TreeNode>>> listTypes(Map<String,?> map){
+        List<String> treeNodePaths = MapUtils.getListFromMapNotNull(map, "treeNodePaths");
+        Map<String,List<TreeNode>> result = new LinkedHashMap<>(treeNodePaths.size());
+        treeNodePaths.forEach(treeNodePath -> {
+            result.put(treeNodePath, treeNodeService.list(null, null, null, null, treeNodePath, null, null, null, null));
+        });
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 查询单种类型字典
+     * @param map
+     * @return
+     */
+    public Result<List<TreeNode>> listType(Map<String,?> map){
+        String treeNodePath = MapUtils.getStringFromMapNotNull(map, "treeNodePath");
+        List<TreeNode> result = treeNodeService.list(null, null, null, null, treeNodePath, null, null, null, null);
+        return ResultUtils.success(result);
+    }
+}
