52d3992954e3754b7d9db2dece91c71046b15b04
王学成 <wxc252@hotmail.com>
2018-09-06 19:26:16 +0800

    初始提交

--- df-admin-static/src/router/index.js
+++ df-admin-static/src/router/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Layout from '@/components/layout/Layout'
 
 Vue.use(Router)
 
+// 异步引入页面
+const page404 = r => require.ensure([], () => r(require('@/views/404')), 'page404')
+const login = r => require.ensure([], () => r(require('@/views/login')), 'login')
+const dashboard = r => require.ensure([], () => r(require('@/views/dashboard/index')), 'dashboard')
+
+const baseRouteMap = [
+  {path: '/login', component: login, hidden: true},
+  {path: '/404', component: page404, hidden: true},
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    name: 'Dashboard',
+    hidden: true,
+    children: [{
+      path: 'dashboard',
+      component: dashboard
+    }]
+  }
+]
+
+const asyncRouteMap = [
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  scrollBehavior: () => ({ y: 0 }),
+  routes: baseRouteMap
 })
+
+export {
+  baseRouteMap,
+  asyncRouteMap
+}
